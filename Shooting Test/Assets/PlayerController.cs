9fcf9c4185e72c54a123d3cda7cf443a42e68bb2
Samuel Borges Ferreira Gomes <samuelbfg@gmail.com>
2015-10-15 16:35:55 -0700

    Shooting Test v1
    Added:
    Unity Shooting Test's project.
    -- Art's Folder: $PlayerJin_Walk.png, BACKGROUND.png, Shuriken.png;
    -- Prefabs' Folder: Shuriken.prefab;
    -- Scenes' Folder: 1.unity;
    -- Background Repeater.cs, Bullet.cs, CameraController.cs,
    PlayerController.cs, TestBackground.cs;
    
    Modified:
    null
    
    Deleted:
    null

---
+++ Shooting Test/Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private Vector3 moveDirection;
+
+    // Use this for initialization
+    void Start () {
+        moveDirection = Vector3.right;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0);
+
+        //EnforceBounds();
+	}
+
+    private void EnforceBounds()
+    {
+        Vector3 newPosition = transform.position;
+        Camera mainCamera = Camera.main;
+        Vector3 cameraPosition = mainCamera.transform.position;
+
+        float xDist = mainCamera.aspect * mainCamera.orthographicSize;
+        float xMax = cameraPosition.x + xDist;
+        float xMin = cameraPosition.x - xDist;
+
+        if (newPosition.x < xMin || newPosition.x > xMax)
+        {
+            newPosition.x = Mathf.Clamp(newPosition.x, xMin, xMax);
+            moveDirection.x = -moveDirection.x;
+        }
+        float yMax = mainCamera.orthographicSize;
+
+        if (newPosition.y < -yMax || newPosition.y > yMax)
+        {
+            newPosition.y = Mathf.Clamp(newPosition.y, -yMax, yMax);
+            moveDirection.y = -moveDirection.y;
+        }
+
+        transform.position = newPosition;
+    }
+}
---
+++ Shooting Test/Assets/TestBackground.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBackground : MonoBehaviour
+{
+    //The speed at which texture scrolls
+    public float scrollSpeed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        //Update the offset value based on time
+        float offset = Time.time * scrollSpeed;
+
+        //Apply the offset value back to the material(This will offset in X axis
+
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
