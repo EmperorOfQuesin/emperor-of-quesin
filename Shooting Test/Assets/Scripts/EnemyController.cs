021795b5aad642c236266014b6ef3c1f21f515ca
Iury <005393228@coyote.csusb.edu>
2015-10-18 23:08:50 -0700

    EoQ v2.2
    Added: Vertical movement to the enemies. The enemies now start moving
    towards the player, after reaching the middle of the screen

--- Shooting Test/Assets/Scripts/EnemyController.cs
+++ Shooting Test/Assets/Scripts/EnemyController.cs
@@ -1,7 +1,7 @@
 ﻿/* 
 Function used to controll the enemy.
 Creator: Samuel Borges
-Collaborators:
+Collaborators: Iury Bizoni
 
 Date of last change: 10/15/2015
 */
@@ -23,11 +23,26 @@ public class EnemyController : MonoBehaviour {
         spawnPoint = GameObject.Find("SpawnPoint").transform;
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
-	}
+    // Update is called once per frame
+    void Update() {
+
+        //The following Variables store the position from the player and from the enemies
+        float playerPos = GameObject.Find("Player").transform.position.y;
+        float enemyPos = GetComponent<Rigidbody2D>().position.y;
+
+        //checks if the enemy has reached the middle of the screen
+        if (GetComponent<Rigidbody2D>().position.x <= 0)
+        {
+            //if the enemy is above the player it will go down, the further it's from the player, the higher its speed will be
+            if(playerPos < enemyPos)
+                GetComponent<Rigidbody2D>().velocity = new Vector2(enemySpeed, 1.5f*(playerPos - enemyPos));
+            //the same as the previous if, but this time it will go up if the enemy is below the player
+            else if(playerPos > enemyPos)
+                GetComponent<Rigidbody2D>().velocity = new Vector2(enemySpeed, 1.5f*(playerPos - enemyPos));
+        }
+
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
