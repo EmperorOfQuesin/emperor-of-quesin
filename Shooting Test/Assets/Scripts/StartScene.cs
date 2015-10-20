2e9c48aa8deacd24b4b6ec9d9d5ef3e90cea1456
Iury <005393228@coyote.csusb.edu>
2015-10-20 00:54:04 -0700

    EoQ v2.4
    Added: A new scene called LaunchScene and a start button, that starts
    the game when clicked.

---
+++ Shooting Test/Assets/Scripts/StartScene.cs
@@ -0,0 +1,33 @@
+﻿/*
+Script used to manage the start button interaction
+Creator: Iury Bizoni
+Colaborator:
+
+Date of last change: 10/19/2015
+*/
+using UnityEngine;
+using System.Collections;
+
+public class StartScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update () {
+       
+	}
+
+    //When clicked the startbutton will load the next scene, starting the game
+    void OnMouseDown()
+    {
+        Invoke("LoadLevel", 0);
+    }
+
+    void LoadLevel()
+    {
+        Application.LoadLevel("1");
+    }
+}
