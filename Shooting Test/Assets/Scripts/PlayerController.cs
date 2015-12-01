d4efedf62cc6753d95231d8a58de7cd3d3cbb6bc
Iury <005393228@coyote.csusb.edu>
2015-11-30 23:02:39 -0800

    Added bunny animation
    Added animation when bunny gets hurt.

--- Shooting Test/Assets/Scripts/PlayerController.cs
+++ Shooting Test/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,9 @@
 ﻿/* 
 Script used to set player's config.
 Creator: Samuel Borges
-Collaborators: Carla Cremon
+Collaborators: Carla Cremon, Iury Bizoni
 
-Date of last change: 11/19/2015
+Date of last change: 11/30/2015
 */
 
 using UnityEngine;
@@ -11,30 +11,32 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	
-	Rigidbody2D rb;
-	//public bool grounded = true;
-	public float jumpPower = 400;
 
-	
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
-	}
+    Rigidbody2D rb;
+    //public bool grounded = true;
+    public float jumpPower = 400;
+
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
-    void Update() {
-		//makes player jump when "space" is pressed"
-	//	if(!grounded && GetComponent<Rigidbody2D>().velocity.y == 0) {
-			//grounded = true;
-		//}
+    void FixedUpdate() {
+        
+        GetComponent<Animator>().SetBool("isHurt", false);
+        //makes player jump when "space" is pressed"
+        //	if(!grounded && GetComponent<Rigidbody2D>().velocity.y == 0) {
+        //grounded = true;
+        //}
 
-	}
-	
-	void OnTriggerEnter2D(Collider2D other)
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-		if (other.tag == "Ground") {
-
+		if (other.tag == "Ground")
+        {
 			if (Input.GetButtonDown("Jump") /*&& grounded == true*/) {
 				Debug.Log("chao");
 				GetComponent<Rigidbody2D>().AddForce(transform.up*jumpPower);
@@ -44,7 +46,7 @@ public class PlayerController : MonoBehaviour {
         if (other.tag == "EnemyWeak")
         {
             HealthManager.HurtPlayer(1);
+            GetComponent<Animator>().SetBool("isHurt", true);
         }
-                
     }
 }
